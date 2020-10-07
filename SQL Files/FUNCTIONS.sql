02a822651b6ca9a8232d9387508bfd1aec614c68
Dhairya <dhairya.ostwal@gmail.com>
2020-10-07 22:14:04 +0530

    Added 2 functions

--- SQL Files/FUNCTIONS.sql
+++ SQL Files/FUNCTIONS.sql
@@ -1,6 +1,6 @@
 -- Creating Functions as stored procedures for important queries
 
--- Query to Calculate Commission earned with BRAND and TYPE as input
+-- Function to Calculate Commission earned with BRAND and TYPE as input
 
 create or replace function get_commission(brand_name text, item_type text)
 returns DECIMAL
@@ -18,3 +18,20 @@ end;
 $$;
 
 -- select get_commission('ZANDU', 'PAINKILL OINTMENT');
+
+-- Function to Calculate Count of payments made using a particular mode
+
+create or replace function get_payment_count(payment_mode text)
+returns int
+LANGUAGE plpgsql
+as
+$$
+DECLARE
+    payment_count integer;
+begin
+    select count(*) into payment_count from payment where payment_method = payment_mode;
+    return payment_count;
+end;
+$$;
+
+-- select get_payment_count('CREDIT/DEBIT CARD');
