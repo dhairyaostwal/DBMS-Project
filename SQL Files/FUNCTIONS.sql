6ca0084f449b62bd72ea6f477fedfc3fed05c09f
Dhairya <dhairya.ostwal@gmail.com>
2020-10-07 22:03:33 +0530

    Function Working

--- SQL Files/FUNCTIONS.sql
+++ SQL Files/FUNCTIONS.sql
@@ -1,20 +1,20 @@
 -- Creating Functions as stored procedures for important queries
 
--- Query to Calculate Commission earned after selling products
--- Display Brand, Type and (Commmission Amount per product)
+-- Query to Calculate Commission earned with BRAND and TYPE as input
 
 create or replace function get_commission(brand_name text, item_type text)
-returns INT
+returns DECIMAL
 LANGUAGE plpgsql
 as
 $$
 DECLARE
-    commission integer;
-    price integer;
+    commission DECIMAL;
+    price DECIMAL;
 begin
-    select commission_percent, cost into commission, price from products where brand = brand_name and type = item_type;
-    return commission *.01 *price;
+    select commission_percent into commission from products where brand = brand_name and type = item_type;
+    select cost into price from products where brand = brand_name and type = item_type;
+    return commission * 0.01 * price;
 end;
 $$;
 
--- Problem with this is it only yields commission_percent from products table
+-- select get_commission('ZANDU', 'PAINKILL OINTMENT');
